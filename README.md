cce86d376e3b843d3b398fd61a37a6cdc3c5a61a
MendelXu <mdxu@hust.edu.cn>
2021-03-09 22:48:36 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,43 @@
 # RelationNet++: Bridging Visual Representations for Object Detection via Transformer Decoder
+by Chi, Cheng and Wei, Fangyun and Hu, Han
+## Introduction
+Existing object detection frameworks are usually built on a single format of objject/part representation, i.e., anchor/proposal rectangle boxes in RetinaNet and
+Faster R-CNN, center points in FCOS and RepPoints, and corner points in CornerNet. While these different representations usually drive the frameworks to perform
+well in different aspects, e.g., better classification or finer localization, it is in general difficult to combine these representations in a single framework to make good
+use of each strength, due to the heterogeneous or non-grid feature extraction by different representations. This paper presents an attention-based decoder module
+similar as that in Transformer to bridge other representations into a typical object detector built on a single representation format, in an end-to-end fashion. The other representations act as a set of key instances to strengthen the main query representation features in the vanilla detectors. Novel techniques are proposed towards efficient computation of the decoder module, including a key sampling approach and a shared location embedding approach. The proposed module is named bridging visual representations (BVR).
 
-Coming soon!
+<div align="center">
+  <img src="demo/rplus.png" width="1178" />
+</div>
 
+## Main Results:
+
+|Model|mAP|AP50|AP75|Link|
+|----|----|---|---|---|
+|[retinanet_bvr_r50](configs/bvr/retinanet_r50_fpn_relation_coco_1x.py)|0.385 |0.591 |0.409|[Google](https://drive.google.com/file/d/1iKygKRi6EmqRsEQgBhJTfToWweVXEltB/view?usp=sharing)|
+|[retinanet_bvr_x101_dcn](configs/bvr/retinanet_x101_dcn_fpn_relation_coco_2x.py)|0.465| 0.663| 0.506|[Google](https://drive.google.com/file/d/1YyAG9OAjkeWStGkM5kLy6l95tXEa_E_b/view?usp=sharing)|
+|[fcos_bvr_x101_dcn](configs/bvr/fcos_x101_dcn_fpn_relation_coco_2x.py)|0.487| 0.680| 0.529|[Google](https://drive.google.com/file/d/1IT1YBnNLrGQs-Be_drfF2ntEq4OjtCaO/view?usp=sharing)|
+|[atss_bvr_x101_dcn](configs/bvr/atss_x101_dcn_fpn_relation_coco_2x.py)|0.506 |0.695| 0.553|[Google](https://drive.google.com/file/d/16kTxTPGIN4O4wFHKhJMdFP_rlZ7eXde9/view?usp=sharing)|
+## How to use it
+### Install it
+```shell
+bash install.sh
+mkdir -p data 
+ln -s ${your_coco_path} data/coco
+```
+where `your_coco_path` is the location of extracted coco dataset on your server. For more information, you may refer to [getting started](https://github.com/open-mmlab/mmdetection/blob/master/docs/get_started.md)
+- ### For testing
+```shell
+bash tools/dist_test.sh ${selected_config} 8
+```
+where `selected_config` is one of provided script under the `config/bvr` folder.
+- ### For training
+```shell
+bash tools/dist_train.sh ${selected_config} 8
+```
+- ### For more dataset
+We have not trained or tested on other dataset. If you would like to use it on other data, please refer to [mmdetection](https://github.com/open-mmlab/mmdetection/blob/master/docs/1_exist_data_model.md).
 ## Citing RelationNet++
 
 ```
@@ -34,3 +70,5 @@ trademarks or logos is subject to and must follow
 [Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
 Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
 Any use of third-party trademarks or logos are subject to those third-party's policies.
+
+
---
+++ configs/bvr/fcos_x101_dcn_fpn_relation_coco_2x.py
@@ -0,0 +1,243 @@
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=dict(backend='zip')),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 400), (1333, 1200)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=dict(backend='zip')),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type='CocoDataset',
+        ann_file='data/coco/annotations/instances_train2017.json',
+        img_prefix='data/coco/train2017/',
+        pipeline=[
+            dict(
+                type='LoadImageFromFile',
+                file_client_args=dict(backend='zip')),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                type='Resize',
+                img_scale=[(1333, 400), (1333, 1200)],
+                multiscale_mode='range',
+                keep_ratio=True),
+            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+        ]),
+    val=dict(
+        type='CocoDataset',
+        ann_file='data/coco/annotations/instances_val2017.json',
+        img_prefix='data/coco/val2017/',
+        pipeline=[
+            dict(
+                type='LoadImageFromFile',
+                file_client_args=dict(backend='zip')),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]),
+    test=dict(
+        type='CocoDataset',
+        ann_file='data/coco/annotations/instances_val2017.json',
+        img_prefix='data/coco/val2017/',
+        pipeline=[
+            dict(
+                type='LoadImageFromFile',
+                file_client_args=dict(backend='zip')),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]))
+evaluation = dict(interval=1, metric='bbox')
+optimizer = dict(
+    type='SGD',
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0001,
+    paramwise_cfg=dict(bias_lr_mult=2.0, bias_decay_mult=0.0))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[16, 19])
+total_epochs = 20
+checkpoint_config = dict(interval=1, create_symlink=False)
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(
+            type='GlobalWandbLoggerHook',
+            init_kwargs=dict(project='keypoint', entity='det'))
+    ])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+model = dict(
+    type='BVR',
+    pretrained='open-mmlab://resnext101_64x4d',
+    backbone=dict(
+        type='ResNeXt',
+        depth=101,
+        groups=64,
+        base_width=4,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        dcn=dict(type='DCNv2', deformable_groups=1, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True),
+        norm_eval=True,
+        with_cp=True,
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_output',
+        num_outs=5),
+    bbox_head=dict(
+        type='BVRHead',
+        bbox_head_cfg=dict(
+            type='FCOSHead',
+            num_classes=80,
+            in_channels=256,
+            stacked_convs=4,
+            feat_channels=256,
+            strides=[8, 16, 32, 64, 128],
+            norm_cfg=None,
+            loss_cls=dict(
+                type='FocalLoss',
+                use_sigmoid=True,
+                gamma=2.0,
+                alpha=0.25,
+                loss_weight=1.0),
+            loss_bbox=dict(type='GIoULoss', loss_weight=1.0),
+            loss_centerness=dict(
+                type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+            norm_on_bbox=True,
+            centerness_on_reg=True,
+            dcn_on_last_conv=True,
+            center_sampling=True,
+            center_sample_radius=1.5,
+            conv_bias=True),
+        keypoint_pos='input',
+        keypoint_head_cfg=dict(
+            type='KeypointHead',
+            num_classes=80,
+            in_channels=256,
+            stacked_convs=2,
+            strides=[8, 16, 32, 64, 128],
+            shared_stacked_convs=0,
+            logits_convs=0,
+            head_types=['top_left_corner', 'bottom_right_corner', 'center'],
+            corner_pooling=False,
+            loss_offset=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0),
+            loss_cls=dict(type='GaussianFocalLoss', loss_weight=0.25)),
+        cls_keypoint_cfg=dict(
+            keypoint_types=['center'],
+            with_key_score=False,
+            with_relation=False),
+        reg_keypoint_cfg=dict(
+            keypoint_types=['top_left_corner', 'bottom_right_corner'],
+            with_key_score=False,
+            with_relation=False),
+        keypoint_cfg=dict(max_keypoint_num=20, keypoint_score_thr=0.0),
+        feature_selection_cfg=dict(
+            selection_method='index',
+            cross_level_topk=50,
+            cross_level_selection=True),
+        num_attn_heads=8,
+        scale_position=False,
+        pos_cfg=dict(base_size=[300, 300], log_scale=True)))
+train_cfg = dict(
+    bbox=dict(
+        assigner=dict(type='ATSSAssigner', topk=9),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    keypoint=dict(
+        assigner=dict(type='PointKptAssigner'),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False))
+test_cfg = dict(
+    nms_pre=1000,
+    min_bbox_size=0,
+    score_thr=0.05,
+    nms=dict(type='nms', iou_threshold=0.6),
+    max_per_img=100)
+gpu_ids = range(0, 8)
